e06a7d498dea427fa19f87d23ae098f55ffac745
Slava Pleshkov <slavapl22@gmail.com>
2023-09-24 23:01:49 +0500

    add class Consts and Encryptor

---
+++ src/constans/Consts.java
@@ -0,0 +1,17 @@
+package constans;
+
+public class Consts {
+
+    private Consts() {
+    }
+    public static final String MAIN_MENU = """
+            Выберите режим работы:
+            1. Шифровка текста
+            2. Расшифровка текста с помощью ключа
+            3. Расшифровка текста с помощью brute force
+            0. Выход из программы""";
+    public static final String PATH_OF_FILE = "Введите путь к файлу";
+    public static final String PATH_OF_ENCRYPTION_FILE = "Введите путь к зашифрованному файлу";
+    public static final String DONE = "Готово!";
+
+}
